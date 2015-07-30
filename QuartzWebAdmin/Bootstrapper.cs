502bf7859ff87f4b4f709276edc8167cb57589fd
unknown <CVicente@J8Q2F12.truphone.local>
2015-07-30 17:18:29 +0100

    Initial commit

---
+++ QuartzWebAdmin/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Nancy.Bootstrappers.Autofac;
+using Nancy.Conventions;
+
+namespace QuartzWebAdmin
+{
+    public class Bootstrapper : AutofacNancyBootstrapper
+    {
+        /// <summary>
+        /// Overrides/configures Nancy's conventions
+        /// </summary>
+        /// <param name="nancyConventions">Convention object instance</param>
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            nancyConventions
+                .StaticContentsConventions
+                .Add(StaticContentConventionBuilder.AddDirectory("extLib", "lib"));
+        }
+    }
+}
---
+++ QuartzWebAdmin/IndexModule.cs
@@ -0,0 +1,14 @@
+﻿namespace QuartzWebAdmin
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters => View["index"];
+            Get["/Jobs"] = parameters => View["index"];
+            Get["/Triggers"] = parameters => View["index"];
+        }
+    }
+}
